14e5fbdc9f2626b878e0f92596eec7ebbcdc8c3e
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-10 09:06:31 +0300

    Update main.py

--- dlink/main.py
+++ dlink/main.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Welcome to my file converter bot! Send me any file and I'll give you a direct download link.")
+    bot.reply_to(message, "Welcome to my dlink bot, Send me file and I will give u direct download link.")
 
 @bot.message_handler(content_types=['document', 'video', 'audio', 'photo'])
 def handle_file(message):
