09e300c4f206d3d89ba7f5c9e87a479939d3efcc
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-16 20:00:19 +0300

    Update main.py

--- dlink/main.py
+++ dlink/main.py
@@ -29,23 +29,28 @@ import time
 bot = telebot.TeleBot('6097684986:AAEHbipv0Sv0hm9LOV7qqZbomHstElFD6Pk')
 admin_id = '5214644649'
 
-@bot.message_handler(commands=['start']) 
-def send_welcome(message): 
-    bot.reply_to(message, "Welcome to Udemy courses coupon bot.") 
- 
+# Send a welcome message when the bot is started.
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Welcome! I will notify you when there are new free Udemy courses available.")
 
+# Function to get the latest free Udemy courses.
 def get_free_course():
     url = 'https://www.real.discount/udemy-coupon/'
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     course_list = soup.find_all('div', {'class': 'coupon-name'})
+    
     for course in course_list:
         coupon = course.find_next_sibling('div', {'class': 'coupon-code'}).text.strip()
         link = course.find('a').get('href')
-        message = f"New free Udemy course: {link}nCoupon code: {coupon}"
+        message = f"New free Udemy course: {link}\nCoupon code: {coupon}"
         bot.send_message(admin_id, message)
 
-# run the function every hour to check for new coupons
+# Run the function every hour to check for new coupons.
 while True:
     get_free_course()
     time.sleep(3600)
+
+# Start polling for messages from users.
+bot.polling()
