956505ab0e9bca827504fa1989e3e0d5164e409d
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-16 19:37:45 +0300

    Update main.py

--- dlink/main.py
+++ dlink/main.py
@@ -1,3 +1,4 @@
+"""
 #bot = ('6115548318:AAF2Sw0DsWUUOVBKoH4VxhvAjLQJStC-zdU'
 import telebot
 import requests
@@ -17,3 +18,28 @@ def handle_file(message):
     bot.reply_to(message, f"Here is ur download link: {file_url}")
 
 bot.polling()
+"""
+
+import telebot
+import requests
+from bs4 import BeautifulSoup
+
+# replace YOUR_API_TOKEN with your Telegram bot API token
+bot = telebot.TeleBot('6097684986:AAEHbipv0Sv0hm9LOV7qqZbomHstElFD6Pk')
+admin_id = '5214644649'
+
+def get_free_course():
+    url = 'https://www.real.discount/udemy-coupon/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    course_list = soup.find_all('div', {'class': 'coupon-name'})
+    for course in course_list:
+        coupon = course.find_next_sibling('div', {'class': 'coupon-code'}).text.strip()
+        link = course.find('a').get('href')
+        message = f"New free Udemy course: {link}nCoupon code: {coupon}"
+        bot.send_message(admin_id, message)
+
+# run the function every hour to check for new coupons
+while True:
+    get_free_course()
+    time.sleep(3600)
